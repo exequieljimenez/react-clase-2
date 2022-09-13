9f9cfc22b5b8a0222193ae346570174bd2250212
exequieljimenez <exequieljimenez@gmail.com>
2022-09-12 21:15:38 -0300

    clase 2 react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
+import AvatarJsx from './components/AvatarJsx';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <img src="https://www.filfre.net/wp-content/uploads/2014/04/amfv.jpg"/>
+        <AvatarJsx />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <button>Boton</button>
+        <input />
         <a
           className="App-link"
           href="https://reactjs.org"
